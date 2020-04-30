edb97259810b946db920e6ad5f20dbdabdfe2556
Héctor Vizcaíno <hektor7@gmail.com>
2020-04-30 18:40:54 +0200

    Using templates inheritance

--- mydjangosite/views.py
+++ mydjangosite/views.py
@@ -20,6 +20,6 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s. </body></html>" % (offset, dt)
-
-    return HttpResponse(html)
+    return render(request, 'future_datetime.html',
+                  {'hour_offset': offset,
+                   'next_time': dt})
