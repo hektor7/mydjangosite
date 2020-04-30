4de8669860d4c3e44c3abc2df3bf1d41dd8001fd
Héctor Vizcaíno <hektor7@gmail.com>
2020-04-30 18:00:51 +0200

    Loading a template

--- mydjangosite/views.py
+++ mydjangosite/views.py
@@ -1,6 +1,8 @@
 import datetime
 
 from django.http import HttpResponse, Http404
+from django.template import Template, Context
+from django.template.loader import get_template
 
 
 def hello(request):
@@ -9,7 +11,8 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    t = get_template('current_datetime.html')
+    html = t.render({'current_date': now})
     return HttpResponse(html)
 
 
