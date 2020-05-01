ab0d3512b9c42a650c103e4d0b0e28ecdbdc3e64
Héctor Vizcaíno <hektor7@gmail.com>
2020-05-01 18:23:10 +0200

    Customize Admin Book page with new class BookAdmin. Search field for Authors

--- mydjangosite/books/admin.py
+++ mydjangosite/books/admin.py
@@ -4,8 +4,16 @@ from .models import Publisher, Author, Book
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date',)
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)
 
 
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
