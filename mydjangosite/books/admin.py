4cd187a79cb201c57a34836baf172a0de64f63dd
Héctor Vizcaíno <hektor7@gmail.com>
2020-05-01 18:09:28 +0200

    String representation for models. Default ordering for Publisher model. Improvements for admin page including AuthorAdmin class. New migration.

--- mydjangosite/books/admin.py
+++ mydjangosite/books/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Publisher, Author, Book
 
-# Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
+
+admin.site.register(Publisher)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book)
